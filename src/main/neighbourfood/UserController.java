78b3a2c2f7f02db3bd9fded1cceb8dff75289770
Risto Rautanen <rautanen.r@gmail.com>
2017-12-08 10:34:14 +0200

    Login tarkistuksen lisäys. Palauttaa nullin jos väärät tunnukset.

--- src/main/neighbourfood/UserController.java
+++ src/main/neighbourfood/UserController.java
@@ -26,6 +26,8 @@ public class UserController {
         userRepository.delete(user);
     }
 
+
+    // Verifies user login and returns either user information, or in case of wrong user details, empty user.
     @PostMapping("/verify")
     public User checkLogin(@RequestBody Verify verify){
         List<User> users = (List<User>)userRepository.findAll();
