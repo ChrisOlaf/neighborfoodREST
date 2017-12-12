87d3ec1c9a3c0bceb840c94d226acae1cd1839ee
piapaula <paulamaki.pia@gmail.com>
2017-12-12 11:14:42 +0200

    Lisätty Salesille requirementsien haku.

--- src/main/neighbourfood/SaleRepository.java
+++ src/main/neighbourfood/SaleRepository.java
@@ -9,4 +9,7 @@ import java.util.List;
 public interface SaleRepository extends CrudRepository<Sale, Integer> {
     @Query("select r from Response r where r.sale_id = :id")
     public List<Response> findAllResponsesForSale(@Param("id") Sale s);
+
+    @Query("select r from Requirement r where r.sale = :id")
+    public List<Requirement> findAllRequirementsForSale(@Param("id") Sale sale_id);
 }
